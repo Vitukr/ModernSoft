f5992941ba017941bf1e34eae3a5094566da361c
Vitalii Yarema <vyarema@windowslive.com>
2018-04-10 13:04:00 +0300

    Ready for Herocu

--- ModernSoft/Program.cs
+++ ModernSoft/Program.cs
@@ -14,7 +14,18 @@ namespace ModernSoft
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            //BuildWebHost(args).Run();
+
+            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseConfiguration(config)
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
