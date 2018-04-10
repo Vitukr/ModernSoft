a2b7917642cc1fe7f815b4fec15aad44cfcc7785
Vitalii Yarema <vyarema@windowslive.com>
2018-04-10 12:42:21 +0300

    Add project files.

---
+++ ModernSoft/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ModernSoft.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
